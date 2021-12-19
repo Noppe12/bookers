090b81bfbc589879475ee834a61cf34d52af76e2
EC2 Default User <ec2-user@ip-172-31-88-250.ec2.internal>
2021-12-19 15:46:59 +0000

    [Finish]課題完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,52 @@ class BooksController < ApplicationController
   def top
   end
 
+  def index
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:success] = "Book was successfully deleted."
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:danger] = "Failed to update."
+      render :edit
+    end
+  end
+
+  def create
+    @books = Book.all.order(id: :asc)
+    @book = Book.new(book_params)
+     if @book.save
+       flash[:success] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+     else
+       render :index
+     end
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_12_14_090644) do
 
-  create_table "booklists", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
